6694bff95a84e0bee3acebad8077e6dbb4b40955
albertgranya <7142036+albertgranya@users.noreply.github.com>
2025-10-14 15:26:57 +0200

    +++

--- components/adc_ads_1256/__init__.py
+++ components/adc_ads_1256/__init__.py
@@ -2,8 +2,17 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
 from esphome.const import CONF_ID
-from esphome.const import CONF_MOSI_PIN,CONF_MISO_PIN,CONF_SS_PIN,CONF_VREF, CONF_CLOCK_MHZ,CONF_DRDY_PIN, CONF_RESET_PIN,CONF_CHIP_SELECT_PIN
-DEPENDENCIES = ["spi"]
+from esphome.const import CONF_MOSI_PIN,CONF_MISO_PIN
+from esphome.const import CONF_SSS_PIN,CONF_VREF, CONF_CLOCK_MHZ,CONF_DRDY_PIN, CONF_RESET_PIN,CONF_CHIP_SELECT_PIN
+
+
+from esphome.core import CORE
+
+CODEOWNERS = ['@albertgranya']
+
+DEPENDENCIES = ['spi','output']
+MULTI_CONF = True
+
 
 adc_ads_1256_ns = cg.esphome_ns.namespace("adc_ads_1256")
 ADCADS1256 = adc_ads_1256_ns.class_(
@@ -23,7 +32,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): _declare_type,
     cv.Optional(CONF_MOSI_PIN, default=18): pins.gpio_output_pin_schema,
     cv.Optional(CONF_MISO_PIN, default=19): pins.gpio_input_pin_schema,
-    cv.Optional(CONF_SS_PIN, default=21): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_SSS_PIN, default=21): pins.gpio_output_pin_schema,
     cv.Optional(CONF_VREF, default=2.5): cv.float_,
     cv.Optional(CONF_CLOCK_MHZ, default=7.68): cv.float_,
     cv.Optional(CONF_DRDY_PIN, default=20): pins.gpio_input_pin_schema,
@@ -45,8 +54,8 @@ async def to_code(config):
     if CONF_MISO_PIN in config:
         miso = await cg.gpio_pin_expression(config[CONF_MISO_PIN])
         cg.add(var.set_miso_pin(miso))
-    if CONF_SS_PIN in config:
-        ss = await cg.gpio_pin_expression(config[CONF_SS_PIN])
+    if CONF_SSS_PIN in config:
+        ss = await cg.gpio_pin_expression(config[CONF_SSS_PIN])
         cg.add(var.set_ss_pin(ss))
     if CONF_VREF in config:
         cg.add(var.set_vref(config[CONF_VREF]))
@@ -62,20 +71,3 @@ async def to_code(config):
         cs = await cg.gpio_pin_expression(config[CONF_CHIP_SELECT_PIN])
         cg.add(var.set_chip_select_pin(cs))
     
-    
-
-    # if CONF_ENABLE_PIN in config:
-    #     enable = await cg.gpio_pin_expression(config[CONF_ENABLE_PIN])
-    #     cg.add(var.set_enable_pin(enable))
-
-    # if CONF_TX_PIN in config:
-    #     tx_pin = await cg.gpio_pin_expression(config[CONF_TX_PIN])
-    #     cg.add(var.set_uart_tx_pin(tx_pin))
-
-    # cg.add(var.set_uart_num(config[CONF_UART_NUM]))
-    # cg.add(var.set_periodic_update(config[CONF_PERIODIC_UPDATE]))
-    # cg.add(var.set_force_full_frames(config[CONF_FORCE_FULL_FRAMES]))
-    # cg.add(var.set_mab_len(config[CONF_CUSTOM_MAB_LEN]))
-    # cg.add(var.set_break_len(config[CONF_CUSTOM_BREAK_LEN]))
-    # cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
-
