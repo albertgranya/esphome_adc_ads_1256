23b9b65558ac15557afbd44faa1658966a19ac50
albertgranya <7142036+albertgranya@users.noreply.github.com>
2025-10-14 15:58:20 +0200

    Update __init__.py

--- components/adc_ads_1256/__init__.py
+++ components/adc_ads_1256/__init__.py
@@ -3,10 +3,11 @@ import esphome.config_validation as cv
 from esphome.components import spi
 from esphome.core import CORE
 from esphome import pins
+from esphome.const import (
+    CONF_ID,CONF_MOSI_PIN,CONF_MISO_PIN,CONF_DRDY_PIN, CONF_RESET_PIN,CONF_CHIP_SELECT_PIN,CONF_SS_PIN,CONF_VREF, CONF_CLOCK_MHZ
+)
 
-from esphome.const import CONF_ID,CONF_MOSI_PIN,CONF_MISO_PIN
-from esphome.const import CONF_DRDY_PIN, CONF_RESET_PIN,CONF_CHIP_SELECT_PIN
-from esphome.const import CONF_SS_PIN,CONF_VREF, CONF_CLOCK_MHZ
+from esphome.const import 
 
 CODEOWNERS = ['@albertgranya']
 
@@ -71,6 +72,7 @@ async def to_code(config):
         cs = await cg.gpio_pin_expression(config[CONF_CHIP_SELECT_PIN])
         cg.add(var.set_chip_select_pin(cs))
     
+
 
 
 
